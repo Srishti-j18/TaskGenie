23e1c90d1a7e5b27001fa6ff161c3d0b52c2233c
Srishti-j18 <srishti33164jaiswal@gmail.com>
2023-10-31 15:26:02 +0530

    added mongodb and finished all of the database operation like get , post, patch and delete

--- api/app.js
+++ api/app.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const app = express();
 
+const { mongoose } = require('./db/mongoose');
+const bodyParser = require('body-parser');
+
+// Load in the mongoose models
+const { List, Task } = require('./db/models');
+
+//Load middleware
+app.use(bodyParser.json());
+
+
+
 // Root Handlers
 
 // list Routes
@@ -9,12 +20,49 @@ const app = express();
 
 
 app.get('/lists', (req, res) => {
-    res.send("Hello World");
     // We want to return an array of all the lists in the database
-})
+    List.find({}).then((lists) => {
+        res.send(lists);
+    });
+});
+
+// Post /lists
+// Purpose: Create a list
 app.post('/lists', (req, res) => {
-    res.send("Hello World");
-    // We want to create a new list and return the new list document back
+    // We want to create a new list and return the new list document back (which includes the Id)
+    // The list information (fields) will be passed in via the JSON request body
+    let title = req.body.title;
+
+    let newList = new List({
+        title
+    });
+    newList.save().then((listDoc) => {
+        // the full list document is returned (incl. id)
+        res.send(listDoc);
+    })
+});
+
+// Post /lists/:id
+// Purpose: Update a specific list
+app.patch('/lists/:id', (req, res) => {
+    // We want to update the specific list (list document with id in the URL) with the new values specified in the JSON body
+    List.findOneAndUpdate({ _id: req.params.id }, {
+        $set: req.body
+    }).then(() => {
+        res.sendStatus(200);
+    });
+});
+
+
+// Post /lists/:id
+// Purpose:  delete a specified list
+app.delete('/lists/:id', (req, res) => {
+    // We want to delete the specified list (document with id in the URL) 
+    List.findOneAndDelete({
+        _id: req.params.id
+    }).then((removedListDoc) => {
+        res.send(removedListDoc);
+    });
 });
 
 app.listen(3000, () => {
