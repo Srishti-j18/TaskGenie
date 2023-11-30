7ca52263894004b8e319629c8bea76707992da24
Srishti-j18 <srishti33164jaiswal@gmail.com>
2023-11-30 21:38:41 +0530

    login-page frontend done

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { TaskViewComponent } from './pages/task-view/task-view.component';
 import { NewListComponent } from './pages/new-list/new-list.component';
 import { NewTaskComponent } from './pages/new-task/new-task.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'lists', pathMatch: 'full' },
   { path: 'new-list', component: NewListComponent },
-  { path: 'lists', component: TaskViewComponent },
+  { path: 'new-list', component: NewListComponent },
+  { path: 'login', component: LoginPageComponent },
   { path: 'lists/:listId', component: TaskViewComponent },
   { path: 'lists/:listId/new-task', component: NewTaskComponent },
 ];
